ee930e232c8ed35f5bf5f928940e4c1f1a10b695
Giorgi Mosemgvdlishvili <g_mosemgvdlishvili@yahoo.com>
2022-06-15 20:45:43 +0400

    add toasting

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -1,82 +1,194 @@
-import React from 'react';
-import { useAddress, useDisconnect, useMetamask } from "@thirdweb-dev/react";
+import React, {useEffect, useState} from 'react';
+import {useAddress, useDisconnect, useMetamask, useNFTDrop} from "@thirdweb-dev/react";
 import {GetServerSideProps} from "next";
 import {sanityClient, urlFor} from "../../sanity";
 import {Collection} from "../../typing";
 import Link from "next/link";
+import {BigNumber} from "ethers";
+import {toast, Toaster} from "react-hot-toast";
 
 interface Props {
     collection: Collection
 }
 
 const NTFDropPage = ({collection}: Props) => {
+    const [claimedSupply, setClaimedSupply] = useState<number>(0)
+    const [totalSupply, setTotalSupply] = useState<BigNumber>()
+    const [priceInEth, setPriceInEth] = useState<string>()
+    const [loading, setLoading] = useState<boolean>(true)
+
+    const ntfDrop = useNFTDrop(collection.address)
 
     const connectWithMetamask = useMetamask();
     const address = useAddress();
     const disconnect = useDisconnect()
 
+    useEffect(() => {
+        if (!ntfDrop) return
+        const fetchPrice = async () => {
+            const claimConditions = await ntfDrop.claimConditions.getAll()
+            setPriceInEth(claimConditions?.[0].currencyMetadata.displayValue)
+        }
+        fetchPrice()
+    }, [ntfDrop])
 
-    console.log("collection", collection)
+    useEffect(() => {
+        if (!ntfDrop) return
+        const fetchNFTDropData = async () => {
+            setLoading(true)
+            const claimed = await ntfDrop.getAllClaimed()
+            const total = await ntfDrop.totalSupply()
+
+            setClaimedSupply(claimed.length)
+            setTotalSupply(total)
+
+            setLoading(false)
+        }
+        fetchNFTDropData()
+    }, [ntfDrop])
+
+    const mintNft = () => {
+        if (!ntfDrop || !address) return
+
+        const quantity = 1
+
+        setLoading(true)
+
+        const notification = toast.loading("Minting...", {
+            style: {
+                background: "white",
+                color: "green",
+                fontWeight: "bolder",
+                fontSize: "17px",
+                padding: "20px"
+            }
+        })
+
+        ntfDrop
+            .claimTo(address, quantity)
+            .then(async (tx) => {
+                const receipt = tx[0].receipt
+                const claimedTokenId = tx[0].id
+                const claimedNFT = await tx[0].data()
+
+                toast("HOORAY.. You Successfully Minted!", {
+                    duration: 8000,
+                    style: {
+                        background: 'green',
+                        color: "white",
+                        fontWeight: "bolder",
+                        fontSize: "17px",
+                        padding: "20px"
+                    }
+                })
+
+            }).catch((err) => {
+                toast("Whoops... Something went wrong", {
+                    style: {
+                        background: 'red',
+                        color: "white",
+                        fontWeight: "bolder",
+                        fontSize: "17px",
+                        padding: "20px"
+                    }
+                })
+        }).finally(() => {
+            setLoading(false)
+            toast.dismiss(notification)
+        })
+    }
+
     return (
         <div className="flex h-screen flex-col lg:grid lg:grid-cols-10">
+            <Toaster position="bottom-center"/>
             {/*    left     */}
-                <div className="lg:col-span-4 bg-gradient-to-br from-cyan-800 to-rose-500 ">
-                    <div className="flex flex-col justify-center items-center py-2 lg:min-h-screen">
-                        <div className="bg-gradient-to-br from-yellow-400 to-purple-600 p-2 ">
-                            <img
-                                className="w-44 rounded-xl object-cover lg:h-96 lg:w-72  "
-                                src="https://links.papareact.com/8sg"
-                                alt="img"
-                            />
-                        </div>
-                        <div className="text-center p-5 space-y-2">
-                            <h1 className="text-4xl font-bold text-white">{collection.ntfCollectionName}</h1>
-                            <h2 className="text-xl text-gray-300">{collection.description}</h2>
-                        </div>
-                    </div>
-                </div>
-
-            {/*    right    */}
-                <div className='flex flex-1 flex-col p-12 lg:col-span-6'>
-                {/*    header   */}
-                    <header className="flex items-center justify-between">
-                        <Link href={"/"}>
-                            <h1 className="w-52 cursor-pointer text-xl font-extralight sm:w-80">
-                                THE{" "}
-                                <span className="font-extrabold underline decoration-pink-600/50" >
-                                PAPAFARM
-                            </span>
-                                {" "}NFT market place
-                            </h1>
-                        </Link>
-                        <button
-                            onClick={() => address ? disconnect() : connectWithMetamask()}
-                            className='rounded-full bg-rose-400 text-white px-4 py-2 text-xs font-bold lg:px-5 lg:py-3 lg:text-base'
-                        >
-                            {address ? "Sign Out" : "Sign In"}
-                        </button>
-                    </header>
-                    <hr className="my-2 border"/>
-                    {address && (
-                        <p className='text-center text-sm text-rose-400'>
-                            Your are logged in with wallet {address.substring(0,5)}...{address.substring(address.length - 5)}
-                        </p>
-                    )}
-                {/*    Content  */}
-                    <div className="mt-10 flex flex-1 flex-col items-center space-y-6 text-center lg:justify-center lg:space-y-0 ">
+            <div className="lg:col-span-4 bg-gradient-to-br from-cyan-800 to-rose-500 ">
+                <div className="flex flex-col justify-center items-center py-2 lg:min-h-screen">
+                    <div className="bg-gradient-to-br from-yellow-400 to-purple-600 p-2 ">
                         <img
-                            className="w-80 object-cover pb-10 lg:h-40"
-                            src={urlFor(collection.mainImage).url()}
+                            className="w-44 rounded-xl object-cover lg:h-96 lg:w-72  "
+                            src="https://links.papareact.com/8sg"
                             alt="img"
                         />
-                        <h1 className="text-3xl font-bold lg:text-5xl lg:font-extrabold">{collection.title}</h1>
-                        <p className="pt-2 text-xl text-green-500 ">13 / 21 NFT is claimed</p>
                     </div>
-                    {/*     MINT Button*/}
-                    <button className="h-16 w-full bg-red-600 text-white font-bold rounded-full mt-10">
-                        Mint NFT (0.01 ETH)
-                    </button>
+                    <div className="text-center p-5 space-y-2">
+                        <h1 className="text-4xl font-bold text-white">{collection.ntfCollectionName}</h1>
+                        <h2 className="text-xl text-gray-300">{collection.description}</h2>
+                    </div>
                 </div>
+            </div>
+
+            {/*    right    */}
+            <div className='flex flex-1 flex-col p-12 lg:col-span-6'>
+                {/*    header   */}
+                <header className="flex items-center justify-between">
+                    <Link href={"/"}>
+                        <h1 className="w-52 cursor-pointer text-xl font-extralight sm:w-80">
+                            THE{" "}
+                            <span className="font-extrabold underline decoration-pink-600/50">
+                                PAPAFARM
+                            </span>
+                            {" "}NFT market place
+                        </h1>
+                    </Link>
+                    <button
+                        onClick={() => address ? disconnect() : connectWithMetamask()}
+                        className='rounded-full bg-rose-400 text-white px-4 py-2 text-xs font-bold lg:px-5 lg:py-3 lg:text-base'
+                    >
+                        {address ? "Sign Out" : "Sign In"}
+                    </button>
+                </header>
+                <hr className="my-2 border"/>
+                {address && (
+                    <p className='text-center text-sm text-rose-400'>
+                        Your are logged in with
+                        wallet {address.substring(0, 5)}...{address.substring(address.length - 5)}
+                    </p>
+                )}
+                {/*    Content  */}
+                <div
+                    className="mt-10 flex flex-1 flex-col items-center space-y-6 text-center lg:justify-center lg:space-y-0 ">
+                    <img
+                        className="w-80 object-cover pb-10 lg:h-40"
+                        src={urlFor(collection.mainImage).url()}
+                        alt="img"
+                    />
+                    <h1 className="text-3xl font-bold lg:text-5xl lg:font-extrabold">{collection.title}</h1>
+                    {loading ? (
+                        <p className="pt-2 text-xl text-green-500 animate-pulse "> Loading Supply Count... </p>
+                    ) : (
+                        <p className="pt-2 text-xl text-green-500 ">{claimedSupply} / {totalSupply?.toString()} NFT is
+                            claimed</p>
+                    )}
+
+                    {loading && (
+                        <img
+                            className="h-80 w-80 object-contain "
+                            src='https://cdn.hackernoon.com/images/0*4Gzjgh9Y7Gu8KEtZ.gif'
+                            alt="loading"
+                        />
+                    )}
+                </div>
+                {/*     MINT Button*/}
+                <button
+                    onClick={mintNft}
+                    disabled={loading || claimedSupply === totalSupply?.toNumber() || !address}
+                    className="h-16 w-full bg-red-600 text-white font-bold rounded-full mt-10 disabled:bg-gray-400"
+                >
+                    {loading ? (
+                        <>loading</>
+                    ) : claimedSupply === totalSupply?.toNumber() ? (
+                        <>SOLD OUT</>
+                    ) : !address ? (
+                        <>Sign in to Mint</>
+                    ) : (
+                        <span className='font-bold'>
+                            Mint NFT ({priceInEth} ETH)
+                        </span>
+                    )}
+
+                </button>
+            </div>
         </div>
     );
 };
@@ -113,7 +225,7 @@ export const getServerSideProps: GetServerSideProps = async ({params}) => {
     const collection = await sanityClient.fetch(query, {
         id: params?.id
     })
-    if  (!collection) {
+    if (!collection) {
         return {
             notFound: true
         }
